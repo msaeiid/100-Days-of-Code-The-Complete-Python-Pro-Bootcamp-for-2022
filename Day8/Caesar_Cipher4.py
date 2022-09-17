53f85d214d417ed649bc7eb6a00cd948c1d1753c
Saeed <mskarbaschian@gmail.com>
2022-09-17 15:03:40 +0430

    Day8

--- Day8/Caesar_Cipher4.py
+++ Day8/Caesar_Cipher4.py
@@ -21,9 +21,12 @@ def caesar(start_text: str, shift_amount: int,cipher_direction: str):
 
 alphabet = [letter for letter in string.ascii_lowercase]
 alphabet+=alphabet
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-if shift > 26:
-    shift = shift%26
-caesar(text,shift,direction)
+go_on='yes'
+while go_on=='yes':
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    if shift > 26:
+        shift = shift%26
+    caesar(text,shift,direction)
+    go_on=input("Type 'yes' if want to go again. Otherwise type 'no'")
