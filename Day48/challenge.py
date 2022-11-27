0e29ec11c736009c8a62839fa12e70196728fdab
Saeed <mskarbaschian@gmail.com>
2022-11-27 19:20:34 +0330

    first challenge

---
+++ Day48/challenge.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+chrome_driver_path = "C:\Development\chromedriver.exe"
+
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+driver.get("https://www.python.org/")
+
+times = driver.find_elements(by=By.CSS_SELECTOR, value=".event-widget time")
+texts = driver.find_elements(by=By.CSS_SELECTOR, value=".event-widget a")
+texts = texts[1:]
+events = {}
+for i in range(len(times)):
+    events[i] = {'time': times[i].text, 'name': texts[i].text}
